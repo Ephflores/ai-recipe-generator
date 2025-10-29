ed04805f662be2b2f470b6c58e16a5d6633e6dba
Ephraim Flores <ephraim.flores@ymail.com>
2025-10-29 16:17:38 -0700

    npx ampx sandbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
+import type { Schema } from "../amplify/data/resource";
 import { useState } from 'react'
+import type { FormEvent } from "react";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
